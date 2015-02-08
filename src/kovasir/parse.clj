aa67e43fcd6ee38f7af9a308d1060912d479af1f
Brandon Bloom <brandon@brandonbloom.name>
2015-02-08 14:30:50 -0500

    simple clojure-like parser

---
+++ src/kovasir/parse.clj
@@ -0,0 +1,74 @@
+(ns kovasir.parse)
+
+(defmulti parse-seq first)
+
+(defn parse [x]
+  (cond
+
+    (or (number? x)
+        (string? x)
+        (keyword? x)
+        (nil? x)
+        (instance? Boolean x)
+        (= () x))
+    {:op :const :value x}
+
+    (symbol? x)
+    {:op :ref :name x}
+
+    (seq? x)
+    (parse-seq x)
+
+    :else
+    {:op :unknown :value x}
+
+  ))
+
+(defmethod parse-seq :default
+  [[f & xs]]
+  {:op :call :f (parse f) :args (mapv parse xs)})
+
+(defmethod parse-seq 'let
+  [[_ bindings & body]]
+  (if-let [[sym init & more] (seq bindings)]
+    {:op :let
+     :sym sym
+     :init (parse init)
+     :expr (parse-seq (list* 'let (vec more) body))}
+    (parse (list* 'do body))))
+
+(defmethod parse-seq 'do
+  [[_ & body]]
+  (if (seq body)
+    (let [expr (-> body last parse)]
+      (if-let [stmts (->> body butlast (map parse) seq)]
+        {:op :do :stmts (vec stmts) :expr expr}
+        expr))
+    {:op :const :value nil}))
+
+(defmethod parse-seq 'if
+  [[_ test then else]]
+  {:op :if
+   :test (parse test)
+   :then (parse then)
+   :else (parse else)})
+
+(comment
+
+  (defn party [x]
+    (-> x parse fipp.edn/pprint))
+
+  (party 1)
+  (party 'x)
+  (party :foo)
+  (party ())
+  (party '((juxt inc dec) (f x) (g y)))
+  (party '(do))
+  (party '(do 1))
+  (party '(do 1 2 3))
+  (party '(let [] (+ x y)))
+  (party '(let [x 1 y 2] (+ x y)))
+  (party '(if 1 2 3))
+  (party '(if 1 2))
+
+)
