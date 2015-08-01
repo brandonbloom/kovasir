c14df791e3b2a83b1472a469845b6b6fa41be40d
Brandon Bloom <brandon@brandonbloom.name>
2015-08-01 13:13:11 -0700

    Full closure semantics!

--- src/kovasir/thorin.clj
+++ src/kovasir/thorin.clj
@@ -1,4 +1,5 @@
-(ns kovasir.thorin)
+(ns kovasir.thorin
+  (:refer-clojure :exclude [eval]))
 
 (defn make-env [prog]
   (reduce (fn [env [label params expr]]
@@ -8,54 +9,63 @@
 
 (def ^:dynamic *trace* false)
 
-(ns-unmap *ns* 'interp*)
-(defmulti interp* (fn [{:keys [env expr]}]
-                    (when *trace*
-                      (prn expr))
-                    (first expr)))
+(ns-unmap *ns* 'step)
+(defmulti step (fn [{:keys [env expr]}]
+                 (when *trace*
+                   (println)
+                   (fipp.edn/pprint env {:width 160})
+                   (prn expr)
+                   (println))
+                 (first expr)))
 
 (def boolean? (partial instance? Boolean))
 
 (defn literal? [x]
   (or (nil? x)
       (number? x)
-      (sequential? x) ;XXX
+      (sequential? x) ;XXX treat seqs and vectors as quoted
+      (map? x) ;XXX self-evaluate closures
       (fn? x)
       (boolean? x)))
 
-(defn lookup [env x]
+(defn closure? [x]
+  (:env x)) ;XXX abuse the map type
+
+(defn eval [x env]
   (cond
     (literal? x) x
     (namespace x) (get-in env [(-> x namespace symbol)
                                :bindings
                                (-> x name symbol)])
-    :else (env x)))
+    :else {:name x :env env}))
 
-(defmethod interp* 'invoke [{[_ f k & args] :expr, :keys [env]}]
+(defmethod step 'invoke [{[_ f k & args] :expr, :keys [env]}]
   {:env env
    :expr (let [f (if (namespace f) ;XXX
-                   (lookup env f)
+                   (eval f env)
                    (resolve f))
-               args (map #(lookup env %) args)]
+               args (map #(eval % env) args)]
            (list k (apply f args)))})
 
-(defmethod interp* 'branch [{[_ b t f] :expr, :keys [env]}]
+(defmethod step 'branch [{[_ b t f] :expr, :keys [env]}]
   {:env env
-   :expr (list (if (lookup env b) t f))})
+   :expr (list (if (eval b env) t f))})
 
-(defmethod interp* 'return [{[_ x] :expr, :keys [env]}]
-  [(lookup env x)])
+(defmethod step 'return [{[_ x] :expr, :keys [env]}]
+  [(eval x env)])
 
-(defmethod interp* :default [{[head & args] :expr, :keys [env]}]
-  (let [{:keys [params expr] :as f} (env head)]
+(defmethod step :default [{[head & args] :expr, :keys [env]}]
+  (let [args (map #(eval % env) args)
+        {:keys [name env], :as f} (eval head env)
+        {:keys [params expr]} (env name)]
     (assert f)
-    {:env (update-in env [head :bindings] into
-                     (map vector params (map #(lookup env %) args)))
+    {:env (update-in env [name :bindings] into
+                     (map vector params args))
      :expr expr}))
 
 (defn interp [prog expr]
   (->> {:env (make-env prog) :expr expr}
-       (iterate interp*)
+       (iterate step)
        next
        (drop-while map?)
        ffirst
@@ -80,6 +90,7 @@
 
 )
 
+;; This version of each invokes host functions.
 (def each '[[each [f xs] (invoke seq k1 each/xs)]
             [k1 [s] (branch k1/s body exit)]
             [body [] (invoke first k2 each/xs)]
@@ -91,5 +102,36 @@
 (comment
 
   (interp each (list 'each prn [1 2 3]))
+
+)
+
+(def both '[
+  ;; Factorial
+  [fac [n k] (frec 1 2)]
+  [frec [r i] (invoke <= fk1 frec/i fac/n)]
+  [fk1 [x1] (branch fk1/x1 fbody fexit)]
+  [fbody [] (invoke * fk2 frec/i frec/r)]
+  [fk2 [x2] (invoke inc fk3 frec/i)]
+  [fk3 [x3] (frec fk2/x2 fk3/x3)]
+  [fexit [] (fac/k frec/r)]
+  ;; Each (this version calls source functions)
+  [each [f xs] (invoke seq ek1 each/xs)]
+  [ek1 [s] (branch ek1/s ebody eexit)]
+  [ebody [] (invoke first ek2 each/xs)]
+  [ek2 [fst] (invoke next ek3 each/xs)]
+  [ek3 [nxt] (each/f ek2/fst ek4)]
+  [ek4 [_] (each each/f ek3/nxt)]
+  [eexit [] (return nil)]
+  ;; Print Each Factorial
+  [pf [x k] (fac pf/x pk)]
+  [pk [y] (invoke prn pf/k pk/y)]
+  [main [] (each pf [1 3 5])]
+])
+
+(comment
+
+  (-> both make-env fipp.edn/pprint)
+
+  (interp both '(main))
 
 )
