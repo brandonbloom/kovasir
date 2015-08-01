a30190a05484a1c501ee31a2f496c1dcdcc20915
Brandon Bloom <brandon@brandonbloom.name>
2015-08-01 16:41:49 -0700

    Tinkering with destination-passing style

---
+++ src/kovasir/dps.clj
@@ -0,0 +1,31 @@
+(ns kovasir.dps)
+
+((fn [n]
+  (loop [r 1 i 2]
+    (if (<= i n)
+      (recur (* r i) (inc i))
+      r)))
+ 5)
+
+
+(def env (atom {}))
+
+(defn fac-dps [n ret]
+  (swap! env assoc
+         :r 1
+         :i 2)
+  (loop []
+    (if (<= (:i @env) n)
+      (do
+        (swap! env assoc
+               :r (* (:r @env) (:i @env))
+               :i (inc (:i @env)))
+        (recur))
+      (swap! env assoc ret (:r @env))))
+  nil)
+
+(do
+  (reset! env {})
+  (fac-dps 5 :ret)
+  (:ret @env))
+
